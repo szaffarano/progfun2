3e2e9605019c0468a4f2d49653998364449a905f
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2017-08-27 16:29:04 -0300

    second attempt

--- assignments/calculator/src/main/scala/calculator/Calculator.scala
+++ assignments/calculator/src/main/scala/calculator/Calculator.scala
@@ -11,10 +11,17 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 object Calculator {
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    def isSafe(n: String, s: Signal[Expr]) = Signal {
-      s() match {
-        case Ref(name) => name != n
-        case _ => true
+    def isSafe(n: String, s: Expr): Signal[Boolean] = {
+      println("preguntando si es seguro " + n + " para " + s)
+      Signal {
+        s match {
+          case Literal(_) => true
+          case Ref(name) => name != n
+          case Plus(a, b) => isSafe(n, a)() && isSafe(n, b)()
+          case Minus(a, b) => isSafe(n, a)() && isSafe(n, b)()
+          case Times(a, b) => isSafe(n, a)() && isSafe(n, b)()
+          case Divide(a, b) => isSafe(n, a)() && isSafe(n, b)()
+        }
       }
     }
 
@@ -22,7 +29,7 @@ object Calculator {
       case (n, s) => {
         if (!namedExpressions.contains(n)) (n, Signal(Double.NaN))
         else  (n, Signal {
-          if (!isSafe(n, s)()) Double.NaN
+          if (!isSafe(n, s())()) Double.NaN
           else eval(s(), namedExpressions)
         })
       }
@@ -30,21 +37,26 @@ object Calculator {
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    expr match  {
-      case Literal(v) => v
-      case Ref(name) => {
-        if (!references.contains(name)) Double.NaN
-        else eval(references(name)(), references)
-      }
-      case Plus(a, b) => eval(a, references) + eval(b, references)
-      case Minus(a, b) => eval(a, references) - eval(b, references)
-      case Times(a, b) => eval(a, references) * eval(b, references)
-      case Divide(a, b) => {
-        val divider = eval(b, references)
-        if (divider == 0) Double.NaN
-        else eval(a, references) / divider
+    def safeEval(evaluated: List[String], expr: Expr): Double = {
+      expr match {
+        case Literal(v) => v
+        case Ref(name) => {
+          if (!references.contains(name)) Double.NaN
+          else if (evaluated.contains(name)) Double.NaN
+          else safeEval(name :: evaluated , references(name)())
+        }
+        case Plus(a, b) => safeEval(evaluated, a) + safeEval(evaluated, b)
+        case Minus(a, b) => safeEval(evaluated, a) - safeEval(evaluated, b)
+        case Times(a, b) => safeEval(evaluated, a) * safeEval(evaluated, b)
+        case Divide(a, b) => {
+          val divider = safeEval(evaluated, b)
+          if (divider == 0) Double.NaN
+          else safeEval(evaluated, a) / divider
+        }
       }
     }
+
+    safeEval(List(), expr)
   }
 
 
@@ -52,7 +64,7 @@ object Calculator {
    *  If the variable is not known, returns a literal NaN.
    */
   private def getReferenceExpr(name: String,
-      references: Map[String, Signal[Expr]]) = {
+      references: Map[String, Signal[Expr]]): Expr = {
     references.get(name).fold[Expr] {
       Literal(Double.NaN)
     } { exprSignal =>
